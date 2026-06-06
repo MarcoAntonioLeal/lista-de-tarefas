c7615a5f40d3a2b6cde9b0fd86b9da27d07d9b8f
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2026-06-06 01:09:43 -0300

    Update criarLista.js

--- public/assets/js/criarLista.js
+++ public/assets/js/criarLista.js
@@ -69,10 +69,12 @@ document.addEventListener('click', event => {
     linhaSelecionada = null
 })
 
-const btnCriar = event.target.closest('.btn-criar')
 const modal = new bootstrap.Modal(document.getElementById('Backdrop'))
 
 formCriarLista.addEventListener('click', event => {
+    const btnCriar = event.target.closest('.btn-criar')
+
+    if(!btnCriar) return
    
     if (tbody.children.length === 0) {
         modal.show()

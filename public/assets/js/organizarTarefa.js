8a3ac7f95f1b5465023a4a222075b1bb211d52d9
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2026-06-29 15:40:37 -0300

    ajustando modal da página de organizar tarefas

--- public/assets/js/organizarTarefa.js
+++ public/assets/js/organizarTarefa.js
@@ -1,1 +1,17 @@
-console.log('Olá, mundo')
+const checkTarefas = document.querySelectorAll('.form-check-input')
+
+const modal = new bootstrap.Modal(document.getElementById('Backdrop'))
+
+formCriarLista.addEventListener('click', event => {
+    const btnCriar = event.target.closest('.btn-criar')
+
+    if(!btnCriar) return
+   
+    if (tbody.children.length === 0) {
+        modal.show()
+        return
+    }
+
+    formCriarLista.submit()
+
+})
